1b79892e5b9e647a6c50300cfba08b03fff54896
Thai Hoang Mai Hoc <maihoc348@gmail.com>
2021-10-23 19:00:31 +0700

    add port server heroku or client 3000

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@
  * $ npm run start-gendoc
  */
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('../swagger_output.json')
-const express = require('express')
-const app = express()
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger_output.json");
+const express = require("express");
+const app = express();
 
 /* Routes */
-const router = require('./routes')
+const router = require("./routes");
 
 /* Middlewares */
-app.use(router)
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(router);
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.listen(3000, () => {
-  console.log("Server is running!\nAPI documentation: http://localhost:3000/doc")
-})
+const port = process.env.PORT || 3000; // port server or 3000
+app.listen(port, () => {
+  console.log(
+    "Server is running!\nAPI documentation: http://localhost:3000/doc"
+  );
+});
